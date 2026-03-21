930e05882123c8886870b43928c18e2b3fbbc8e1
Biruk Zenebe <148862641+BirukZenebe1@users.noreply.github.com>
2026-03-21 08:18:56 +0100

    Update image source in Portfolio component

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -167,7 +167,7 @@ export default function Portfolio() {
             transition={{ delay: 0.15 }}
           >
             <img
-              src="https://i.postimg.cc/7YPJYTF7/IMG-4011-2.avif"
+              src="https://i.postimg.cc/nrq5Msk5/IMG-8122.png"
               alt="Biruk Zenebe"
               className="h-full w-full object-cover"
             />
